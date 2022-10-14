e7b309134eb71ea2bba551f66ecad3c8543661cc
Daveolik <114314859+Daveolik@users.noreply.github.com>
2022-10-14 17:23:28 +0400

    add count of product

---
+++ script/script.js
@@ -0,0 +1,66 @@
+const catalogItems = document.querySelectorAll('.catalog-item');
+       
+let count = 1;
+
+const checkCount = countItem => {
+  const plusBtn = countItem.querySelector('.plus-btn'),
+        minusBtn = countItem.querySelector('.minus-btn'),
+        spanCalc = countItem.querySelector('.btns-calc-wrapper span'),
+        sum = countItem.querySelector('.wrapper-info span'),
+        startCalcValue = parseFloat(sum.textContent),
+        btnBuy = countItem.querySelector('.btns-wrapper > button'),
+        calcWrapper = countItem.querySelector('.wrapper-calc-form'),
+        total = calcWrapper.querySelector('.total span'),
+        checkboxes = calcWrapper.querySelectorAll('input[type="checkbox"]'),
+        btnClose = countItem.querySelector('.icon-close');
+ 
+  const countPlus = () => {
+    count++;
+    spanCalc.textContent = count;
+    sum.textContent  = `${(startCalcValue * count)} ₽`;
+  };
+
+  const countMinus = () => {
+    if (count > 1) {
+        count--;
+        sum.textContent = `${parseFloat(sum.textContent) - startCalcValue} ₽`;
+    }
+    
+    spanCalc.textContent = count;
+  };
+
+  plusBtn.addEventListener('click', countPlus);
+  minusBtn.addEventListener('click', countMinus);
+
+  btnBuy.addEventListener('click', () => {
+    calcWrapper.style.cssText= `
+    display:flex;
+    flex-direction: column;
+  `;
+  
+  total.textContent = `${parseFloat(sum.textContent)} ₽`;
+ 
+  checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', () => {
+    if (checkbox.checked) {
+      total.textContent = `${parseFloat(total.textContent) + parseFloat(checkbox.value)} ₽`;
+    } else {
+      total.textContent = `${parseFloat(total.textContent) - parseFloat(checkbox.value)} ₽`;
+     } 
+   });
+  });
+  
+ });
+
+  btnClose.addEventListener('click', () => {
+    calcWrapper.style.display = 'none';
+
+    checkboxes.forEach(checkbox => {
+       if (checkbox.checked) {
+          checkbox.checked = false;
+       }
+     });
+  });
+};
+
+catalogItems.forEach(checkCount);
